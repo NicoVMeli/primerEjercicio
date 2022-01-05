b7b224e2c20d4d094d76aeb0e85bb734e7ab8fa1
Nico Vega <nicolas.vega@mercadolibre.com>
2022-01-05 20:52:27 -0300

    agregando gin

--- main.go
+++ main.go
@@ -1,13 +1,3 @@
-/*
-echo "# primerEjercicio" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/NicoVMeli/primerEjercicio.git
-git push -u origin main
-*/
-
 package main
 
 import (
